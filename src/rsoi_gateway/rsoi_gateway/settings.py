e294c5fb8f87f007c835a4f51a66881175cfd49f
qwerty14228 <shapranmatvey@gmail.com>
2025-01-11 13:16:25 +0300

    правки

--- src/rsoi_gateway/rsoi_gateway/settings.py
+++ src/rsoi_gateway/rsoi_gateway/settings.py
@@ -142,13 +142,16 @@ else:
 
 APPEND_SLASH = False
 
-RQ_QUEUES = {
-    'default': {
-        'HOST': environ['REDIS_HOST'],
-        'PORT': 6379,
-        'DB': 0,
-        'USERNAME': environ['REDIS_USER'],
-        'PASSWORD': environ['REDIS_PASSWORD'],
-        'DEFAULT_TIMEOUT': 360,
-    },
-}
+if 'RUN_UNIT_TESTS' in environ:
+    RQ_QUEUES = {}
+else:
+    RQ_QUEUES = {
+        'default': {
+            'HOST': environ['REDIS_HOST'],
+            'PORT': 6379,
+            'DB': 0,
+            'USERNAME': environ['REDIS_USER'],
+            'PASSWORD': environ['REDIS_PASSWORD'],
+            'DEFAULT_TIMEOUT': 360,
+        },
+    }
